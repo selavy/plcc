c8877f81604f1e982d73beeb193664983c44f02c
Peter Lesslie <pclesslie@gmail.com>
2020-03-27 16:25:41 -0500

    Add Catch2 and command line parser

--- src/plcc.cpp
+++ src/plcc.cpp
@@ -1,5 +1,30 @@
+#include <iostream>
 #include <cstdio>
+#include <vector>
+#include <string>
+#include <cxxopts.hpp>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
+int main(int argc, char **argv) {
+    cxxopts::Options options("plcc", "simple c compiler");
+    options.add_options()
+        ("sources", "input source files", cxxopts::value<std::vector<std::string>>())
+        ("h,help", "print usage")
+        ;
+    options.parse_positional({ "sources" });
+    auto args = options.parse(argc, argv);
+
+    if (!args.count("sources")) {
+        std::cerr << "fatal error: no input files\n\n"
+            << options.help()
+            << std::endl;
+        return 1;
+    }
+
+    auto sources = args["sources"].as<std::vector<std::string>>();
+    for (const auto& source : sources) {
+        std::cout << "Compiling '" << source << "'...\n";
+    }
+
+    std::cout << "Bye." << std::endl;
     return 0;
 }
---
+++ tests/first_test.cpp
@@ -0,0 +1,6 @@
+#include <catch2/catch.hpp>
+
+TEST_CASE("First test", "the first test!")
+{
+    REQUIRE(1 == 2);
+}
